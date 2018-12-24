6bc42918c763497400001260b9226985abc1f7b6
Lazaros Adaloglou <adalogloulazaros@gmail.com>
2018-12-24 04:12:05 +0200

    Delete AvrBuild.bat

--- AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"D:\STUDY\OTHER\AVRSTUDIO\AvrAssembler2\avrasm2.exe" -S "C:\Users\removevirus\Documents\Final\labels.tmp" -fI -W+ie -C V2E -o "C:\Users\removevirus\Documents\Final\Final.hex" -d "C:\Users\removevirus\Documents\Final\Final.obj" -e "C:\Users\removevirus\Documents\Final\Final.eep" -m "C:\Users\removevirus\Documents\Final\Final.map" "C:\Users\removevirus\Documents\Final\Final.asm"
